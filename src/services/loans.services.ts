fa699d874d4b73b2094a030b9f95a819c8e9a09a
Atala2000 <atalakidi@gmail.com>
2024-06-21 13:25:11 +0300

    refactor: Remove unused createNewExpense function and add calcSimpleInterest and calcCompoundInterest functions

--- src/services/loans.services.ts
+++ src/services/loans.services.ts
@@ -1,12 +1,7 @@
-import mongoose from "mongoose";
-import {User, Expenses, Loans, ModelInterface} from '../models/models';
-import {connectToDatabase, saveModelData } from './db.service'
-
-const createNewExpense = async (expense: ModelInterface): Promise<void> => {
-    try {
-        await saveModelData(expense);
-        console.log("Successfuly saved model");
-    } catch(error) {
-        console.log(`Error saving data: ${error}`);
-    }
-}
+const calcSimpleInterest = (amount: number, time: number, rate: number): number => {
+    return amount * rate * time;
+  };
+  
+  const calcCompoundInterest = (amount: number, time: number, rate: number): number => {
+    return amount * Math.pow((1 + rate), time) - amount;
+  };
