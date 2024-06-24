c290896e3594b8ffff8f6c8afd21242e8b3db157
Atala2000 <atalakidi@gmail.com>
2024-06-24 23:23:35 +0300

    Refactor routes to use PUT method for updating expenses and loans

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,27 @@
-import { createExpense, findExpense, deleteExpense, updateExpense } from '../controllers/ExpenseController';
-import { createLoan, findLoan, deleteLoan, updateLoan } from '../controllers/LoansController';
+import {
+  createExpense,
+  findExpense,
+  deleteExpense,
+  updateExpense,
+} from "../controllers/ExpenseController";
+import {
+  createLoan,
+  findLoan,
+  deleteLoan,
+  updateLoan,
+} from "../controllers/LoansController";
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Routes for expenses
-router.post("/expense", createExpense)
+router.post("/expense", createExpense);
 router.get("/expense/:expenseId", findExpense);
-router.post("/expense/:expenseId", updateExpense);
-router.post("/expense/:expenseId/delete", deleteExpense);
+router.put("/expense/:expenseId", updateExpense);
+router.put("/expense/:expenseId/delete", deleteExpense);
 
 // Routes for loans
-router.post("/loan", createLoan)
+router.post("/loan", createLoan);
 router.get("/loan/:expenseId", findLoan);
-router.post("/loan/:loanId", updateLoan);
-router.post("/loan/:loanId/delete", deleteLoan);
+router.put("/loan/:loanId", updateLoan);
+router.put("/loan/:loanId/delete", deleteLoan);
