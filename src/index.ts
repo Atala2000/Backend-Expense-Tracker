c47f27dd6a74d7d30f75dd087a17da7e6672c2d7
Atala2000 <atalakidi@gmail.com>
2024-06-25 00:12:05 +0300

    refactor: Update routes in index.ts to use express router

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import express, {Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import router from "./routes";
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
- res.send("Our servers");
-})
+app.use("/", router);
+app.get("/test", (req: Request, res: Response) => {
+  return res.send("Servier works");
+});
 
 app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`)
-})
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
