c7816bed7524799561a94f828d79d517eea233c8
Atala2000 <atalakidi@gmail.com>
2024-08-12 17:50:42 +0300

    refactor: Update session handling in index.ts to use express-session and connect-mongo

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,34 @@
 import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-import router from "./routes";
+import * as dotenv from "dotenv";
+import session from 'express-session';
+import router from './routes/routes';
+import process from "process";
+import MongoStore from "connect-mongo";
 
 dotenv.config();
 
+
 const app = express();
 
+
+app.use(session({
+  secret: process.env.SECRET_KEY as string,
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongoUrl: 'mongodb://127.0.0.1:27017/session-store'  
+  }),
+  cookie: {
+    secure: false,
+    sameSite: 'strict'
+  }
+}))
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.use("/", router);
 app.get("/test", (req: Request, res: Response) => {
-  return res.send("Servier works");
+  return res.send("Server works");
 });
 
 app.listen(port, () => {
